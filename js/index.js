d9b8480a1f6b992eeedef390a6f8b3ef1cf25085
Shikha Sharma <162238672+Shar31@users.noreply.github.com>
2024-05-16 10:24:05 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,70 @@
+// My first Assignment
+// There are 2 taks starting from 4th line is task one and from line 48 is task two.
+
+// Task 01
+//__________________________________
+//Refference provided by proff
+// alert( "a + b: " + (a + b) );
+/*
+a + b
+a - b
+a * b
+a / b
+a % b
+a += b
+a -= b
+a *= b
+a /= b
+a %= b
+a == b
+a != b
+a > b
+a < b
+!a && !c 
+!a || !c 
+*/
+// Task follows here
+let a = 3;
+let b = 5;
+let c;
+
+
+// FOllow this foer all 
+let result = `
+    let a = 3;
+    let b = 5;
+    let c;
+   ----------------
+    a + b = ${a + b}
+    a - b = ${a - b}
+    a * b = ${a * b}
+    a / b = ${a / b}
+    a % b = ${a % b}
+    a += b = ${a += b}
+    a -= b = ${a -= b} 
+    a *= b = ${a *= b}
+    a /= b = ${a /= b}
+    a %= b = ${a %= b}
+    a == b = ${a == b}
+    a != b = ${a != b}
+    a > b = ${a > b}
+    a < b = ${a < b}
+    !a && !c = ${!a && !c}
+    !a || !c = ${!a || !c}
+`;
+console.log(result);
+
+// Task 02 Declare the variable first_name and assign it with the value of your first name;
+// Declare the variable last_name and assign it with the value of your last name;
+// Declare the variable email and assign it with the value of your email;
+// Declare the variable output;
+//____________________
+
+let f_name = "Shikha";
+let l_name = "Sharma";
+let email = "shar0734@algonquinlive.com";
+let output = `My name is ${f_name} ${l_name}. You can contact me at ${email}.`;
+
+//Print on console
+console.log(output);
+
